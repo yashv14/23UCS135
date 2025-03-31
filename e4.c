9ab6e7cad0ae039580537031bcd18079ea831c6c
yashv14 <yvasagadekar@gmail.com>
2025-03-31 22:38:20 +0530

    Add files via upload

---
+++ e4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quick_sort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int num[] = {10, 7, 8, 9, 1, 5};
+    int size = sizeof(num) / sizeof(num[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", num[i]);
+    }
+    printf("\n");
+
+    quick_sort(num, 0, size - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", num[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
